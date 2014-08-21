2537d479aa5c40bfacaf50c3eff41890a7f38eb5
Robin Southern <betajaen@ihoed.com>
2014-08-21 09:48:07 +0100

    More boxes on the Rectangles Example Added screenshots for each example

--- Examples/1-Rectangles/Rectangles.cs
+++ Examples/1-Rectangles/Rectangles.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Rectangles : MonoBehaviour
 {
@@ -20,18 +19,20 @@ public class Rectangles : MonoBehaviour
     mBoxes[2] = mAtlas.GetSprite("Box3");
     mBoxes[3] = mAtlas.GetSprite("Box4");
 
-    mLayer.Begin(4);
-    mLayer.Quad(10, 10, mBoxes[0]);
-    mLayer.Quad(100, 10, mBoxes[1]);
-    mLayer.Quad(200, 10, mBoxes[2]);
-    mLayer.Quad(30, 70, mBoxes[3]);
+    int count = 50 + UnityEngine.Random.Range(1, 20);
+
+    mLayer.Begin(count);
+
+    for (int i = 0; i < count; i++)
+    {
+      int x = UnityEngine.Random.Range(0, Screen.width);
+      int y = UnityEngine.Random.Range(0, Screen.height);
+      int b = UnityEngine.Random.Range(0, mBoxes.Length);
+      mLayer.Quad(x, y, mBoxes[b]);
+    }
+
     mLayer.End();
 
   }
 
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
 }
